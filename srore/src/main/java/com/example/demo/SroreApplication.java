e1b76f6ad6451574d7c33995541a6d482380eb8d
jhta <gyulbbe@gmail.com>
2024-10-21 09:20:39 +0900

    first commit

---
+++ srore/src/main/java/com/example/demo/SroreApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SroreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SroreApplication.class, args);
+	}
+
+}
