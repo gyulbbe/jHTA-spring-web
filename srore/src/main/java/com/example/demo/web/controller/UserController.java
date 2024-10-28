421010d85f2147d57f473e22bb2d82d02c374cb1
jhta <gyulbbe@gmail.com>
2024-10-28 11:52:25 +0900

    UsernamePasswordAuthenticationToken에 있는 principal을 형변환 없이 간단하게 꺼내기

--- srore/src/main/java/com/example/demo/web/controller/UserController.java
+++ srore/src/main/java/com/example/demo/web/controller/UserController.java
@@ -1,22 +1,21 @@
 package com.example.demo.web.controller;
 
-import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.example.demo.security.CustomUserDetails;
+import com.example.demo.security.LoginUser;
 
 @Controller
 @RequestMapping("/my")
 public class UserController {
 
 	@GetMapping("/info")
-	public String detail(Authentication authentication) {
-		CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
-		System.out.println("사용자 번호: " + userDetails.getNo());
-		System.out.println("닉네임: " + userDetails.getNickname());
-
+	public String detail(@AuthenticationPrincipal LoginUser loginUser) {
+		System.out.println("사용자 번호: " + loginUser.getNo());
+		System.out.println("사용자 이메일: " + loginUser.getEmail());
+		System.out.println("사용자 닉네임: " + loginUser.getNickname());
 		return "user/detail";
 	}
 }
